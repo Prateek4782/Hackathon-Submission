f5a19c8e7fee9b458dd11e12991aa37b4cc880c0
Prateek4782 <106441817+Prateek4782@users.noreply.github.com>
2023-04-03 18:45:26 +0530

    styling done except cards

--- src/Components/Description/Description.js
+++ src/Components/Description/Description.js
@@ -1,21 +1,40 @@
 import React from 'react';
-import { BsCalendar } from "react-icons/bs";
+import { BsCalendar } from 'react-icons/bs';
+import {AiFillGithub} from 'react-icons/ai'
+import{BiLinkExternal} from 'react-icons/bi'
 export const Description = ({ description, hackathonName, formattedStartDate, formattedEndDate }) => {
-  
+  const startDate = new Date(formattedStartDate);
+  const endDate = new Date(formattedEndDate);
+
+  const getFormattedDate = (date) => {
+    const day = date.getDate();
+    const month = date.toLocaleString('default', { month: 'long' });
+    const year = date.getFullYear();
+    return `${day} ${month} ${year}`;
+  };
+
   return (
     <div className="container">
       <div className="desc-container">
         <p className="desc-hackathon-p">Description</p>
-        <p>
-          {description}
-        </p>
+        <p>{description}</p>
       </div>
       <div className="side-container">
         <p className="desc-hackathon-p hack">Hackathon</p>
-        <p className='desc-hackathon-p name' >{hackathonName}</p>
-        <p className='desc-hackathon-p hack date'><BsCalendar/>{formattedStartDate} - {formattedEndDate}</p>
-        <button>Github links</button>
-        <button>Other Links</button>
+        <p className="desc-hackathon-p name">{hackathonName}</p>
+        <div className="date-container">
+          <BsCalendar className="date-icon" />
+          <p className="desc-hackathon-p hack date">
+            {getFormattedDate(startDate)} - {getFormattedDate(endDate)}
+          </p>
+        </div>
+        <div className="desc-btn-container">
+  <div className="desc-btn">
+    <button><AiFillGithub/><span>Github links</span></button>
+    <button><BiLinkExternal/><span>Other Links</span></button>
+  </div>
+</div>
+
       </div>
     </div>
   );
