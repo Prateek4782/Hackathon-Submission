16fc99070cf72edc3865bd5c30071d8a6ed83623
Prateek4782 <106441817+Prateek4782@users.noreply.github.com>
2023-04-03 19:40:05 +0530

    Done almost testing 1

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,20 +1,30 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import {BiSearchAlt2} from 'react-icons/bi'
 export const Cards = () => {
   const formDataList = JSON.parse(localStorage.getItem('formDataList')) || [];
   const [searchValue, setSearchValue] = useState('');
   const [sortOrder, setSortOrder] = useState('newest'); // default sort order is by newest
 
   const filteredFormDataList = formDataList
-    .filter(formData => formData.title.toLowerCase().includes(searchValue.toLowerCase()))
-    .sort((a, b) => {
-      if (sortOrder === 'oldest') {
-        return new Date(a.date) - new Date(b.date);
-      } else {
-        return new Date(b.date) - new Date(a.date);
-      }
-    });
+  .filter(formData => formData.title.toLowerCase().includes(searchValue.toLowerCase()))
+  .map(formData => {
+    const lastUpdatedDaysAgo = Math.floor(
+      (new Date() - new Date(formData.date)) / (1000 * 60 * 60 * 24)
+    );
+    return {
+      ...formData,
+      lastUpdatedDaysAgo,
+    };
+  })
+  .sort((a, b) => {
+    if (sortOrder === 'oldest') {
+      return new Date(a.date) - new Date(b.date);
+    } else {
+      return new Date(b.date) - new Date(a.date);
+    }
+  });
+
 
   const handleSearchInputChange = event => {
     setSearchValue(event.target.value);
@@ -27,12 +37,14 @@ export const Cards = () => {
   return (
     <>
       <div className="searchbar ">
+      <BiSearchAlt2 className="search-icon" />
         <input
+        
           type="search"
-          placeholder="Search by title"
+          placeholder="Search "
           value={searchValue}
           onChange={handleSearchInputChange}
-        />
+          />
 
         <select  value={sortOrder} onChange={handleSortChange} className='option'>
           <option value="newest">Newest</option>
@@ -55,7 +67,8 @@ export const Cards = () => {
                 <h2>{formData.title}</h2>
               </div>
               <p>{formData.summary}</p>
-              <p className='card-date'>{formData.date}</p>
+              <p className='card-date'> <em> Uploaded {formData.lastUpdatedDaysAgo} days ago</em> </p>
+
             </div>
           </Link>
         ))}
