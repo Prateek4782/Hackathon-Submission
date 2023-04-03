be1fb222eeb7fef2b1d901c18f8cd2e9856bad3f
Prateek4782 <106441817+Prateek4782@users.noreply.github.com>
2023-04-03 19:49:22 +0530

    Done final deployement

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import {Link, useLocation} from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 export const Navigation = () => {
   const location = useLocation();
-  
+
   const isHomePage = location.pathname === '/';
-  
+
   return (
     <>
       <div className="bar">
         <div className="text-navigation">
           <Link to={'/'}>
-            <h5 className={isHomePage ? "border-bot" : ""}>All Submissions</h5>
+            <h5 className={isHomePage ? "border-bot active" : "inactive"}>All Submissions</h5>
           </Link>
           <Link to={'/Fav'}>
-            <h5 className={!isHomePage ? "border-bot" : ""}>Favourite Submissions</h5>
+            <h5 className={!isHomePage ? "border-bot active" : "inactive"}>Favourite Submissions</h5>
           </Link>
         </div>
       </div>
